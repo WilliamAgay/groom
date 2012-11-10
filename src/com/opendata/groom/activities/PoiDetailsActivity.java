49c4ee02aef863604201327d05b46d9bdc250124
Audrey Martel <adry.ma@gmail.com>
2012-11-10 19:26:34 +0100

    ok

---
+++ src/com/opendata/groom/activities/PoiDetailsActivity.java
@@ -0,0 +1,26 @@
+package com.opendata.groom.activities;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.opendata.groom.R;
+
+public class PoiDetailsActivity extends Activity {
+
+	
+	
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.poi_details_activity);
+
+	
+
+		
+		
+	}
+	
+
+}
