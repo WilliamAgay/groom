f4b78143f7d92b8609acd4e5aa5705ec439fb80a
Audrey Martel <adry.ma@gmail.com>
2012-11-10 11:12:39 +0100

    udrey 2

---
+++ src/com/opendata/groom/GroomApplication.java
@@ -0,0 +1,13 @@
+package com.opendata.groom;
+
+import android.app.Application;
+
+public class GroomApplication extends Application{
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+
+}
--- src/com/opendata/groom/activities/InitActivity.java
+++ src/com/opendata/groom/activities/InitActivity.java
@@ -1,7 +1,27 @@
 package com.opendata.groom.activities;
 
-import android.app.Activity;
 
-public class InitActivity extends Activity{
+import com.opendata.groom.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InitActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.init_activity);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+	
+	
 
 }
