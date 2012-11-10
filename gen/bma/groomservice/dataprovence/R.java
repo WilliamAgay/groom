c7b69569143d476c68c1de99620c74989a678e05
Audrey Martel <adry.ma@gmail.com>
2012-11-10 16:10:02 +0100

    layout init

--- gen/bma/groomservice/dataprovence/R.java
+++ gen/bma/groomservice/dataprovence/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.opendata.groom;
+package bma.groomservice.dataprovence;
 
 public final class R {
     public static final class anim {
--- src/com/opendata/groom/GroomApplication.java
+++ src/com/opendata/groom/GroomApplication.java
@@ -1,20 +1,24 @@
 package com.opendata.groom;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import bma.groomservice.data.Poi;
+import bma.groomservice.data.dataprovence.DataprovenceFileHelper;
 
 public class GroomApplication extends Application{
 
 	public static final String PREF_ACCOUNT_BUDGET = "budget";
 	public static final String PREF_ACCOUNT_NAME = "name";
 	public static final String PREF_ACCOUNT_STATUS = "status";
+	public static final String PREF_FAVORITES = "favorites";
 	
 	
 	public String accountName=  null;
@@ -28,6 +32,24 @@ public class GroomApplication extends Application{
 	{
 		super.onCreate();
 		readAccountDataInPref();
+//		List<Poi> pois = new DataprovenceFileHelper(
+//				"RestaurantsGastronomiques.json").find(null);
+				;
+//		ArrayList prefs =  new ArrayList<String>();
+//		prefs.add(object);
+//		
+//		
+//		{"raisonsociale" = p.readString();
+//		"tlphone" = p.readString();
+//		"adresseWeb" = p.readString();
+//		"latitude" = p.readDouble();
+//		"longitude" = p.readDouble();
+//		"codepostal" = p.readString();
+//		"type" = p.readString();
+//		"ville" = p.readString();
+//		"voie" = p.readString();}
+//		
+//		setStringArrayPref(this, PREF_FAVORITES, prefs);
 	}
 	
 	
@@ -56,5 +78,37 @@ public class GroomApplication extends Application{
 	    editor.commit();
 	}
 	
+	public static ArrayList<String> getStringArrayPref(Context context, String key) {
+	    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		
+	    String json = prefs.getString(key, null);
+	    ArrayList<String> urls = new ArrayList<String>();
+	    if (json != null) {
+	        try {
+	            JSONArray a = new JSONArray(json);
+	            for (int i = 0; i < a.length(); i++) {
+	                String url = a.optString(i);
+	                urls.add(url);
+	            }
+	        } catch (JSONException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return urls;
+	}
+	public static void setStringArrayPref(Context context, String key, ArrayList<String> values) {
+	    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+	    SharedPreferences.Editor editor = prefs.edit();
+	    JSONArray a = new JSONArray();
+	    for (int i = 0; i < values.size(); i++) {
+	        a.put(values.get(i));
+	    }
+	    if (!values.isEmpty()) {
+	        editor.putString(key, a.toString());
+	    } else {
+	        editor.putString(key, null);
+	    }
+	    editor.commit();
+	}
 
 }
