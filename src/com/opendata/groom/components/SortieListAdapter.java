40c35844c64ae208a0b92a90ae38a3d1310ed103
nicobo <nicobo@metrast>
2012-11-10 21:57:24 +0100

    + début de detail poi

--- src/com/opendata/groom/components/SortieListAdapter.java
+++ src/com/opendata/groom/components/SortieListAdapter.java
@@ -12,50 +12,56 @@ import bma.groomservice.data.Poi;
 import com.opendata.groom.GroomApplication;
 import com.opendata.groom.R;
 
-
 public class SortieListAdapter extends BaseAdapter {
-  private final Context context;
-  
-//  private ArrayList<Poi> poiList = new ArrayList<Poi>();
+	private final Context context;
 
-  public SortieListAdapter(Context context) {
-    this.context = context;
-//    this.poiList=pois;
-  }
+	// private ArrayList<Poi> poiList = new ArrayList<Poi>();
 
-  @Override
-  public int getCount() {
-    return  ((GroomApplication) this.context.getApplicationContext()).pois.size();
-  }
+	public SortieListAdapter(Context context) {
+		this.context = context;
+		// this.poiList=pois;
+	}
 
-  @Override
-  public Object getItem(int index) {
-    // Not used, so no point in retrieving it.
-    return null;
-  }
+	@Override
+	public int getCount() {
+		return ((GroomApplication) this.context.getApplicationContext()).pois
+				.size();
+	}
 
-  @Override
-  public long getItemId(int index) {
-    return index;
-  }
+	@Override
+	public Object getItem(int index) {
+		// Not used, so no point in retrieving it.
+		return null;
+	}
 
-  @Override
-  public View getView(int index, View view, ViewGroup viewGroup) {
-    RelativeLayout layout;
-    if (view instanceof RelativeLayout) {
-      layout = (RelativeLayout) view;
-    } else {
-      LayoutInflater factory = LayoutInflater.from(context);
-      layout = (RelativeLayout) factory.inflate(R.layout.sortie_list_item, viewGroup, false);
-    }
+	@Override
+	public long getItemId(int index) {
+		return index;
+	}
 
-   Poi poi= ((GroomApplication) this.context.getApplicationContext()).pois.get(index);
+	@Override
+	public View getView(int index, View view, ViewGroup viewGroup) {
+		RelativeLayout layout;
+		if (view instanceof RelativeLayout) {
+			layout = (RelativeLayout) view;
+		} else {
+			LayoutInflater factory = LayoutInflater.from(context);
+			layout = (RelativeLayout) factory.inflate(
+					R.layout.sortie_list_item, viewGroup, false);
+		}
 
+		Poi poi = ((GroomApplication) this.context.getApplicationContext()).pois
+				.get(index);
 
-      ((TextView) layout.findViewById(R.id.TextViewSortieListItemTitle)).setText(poi.type);
-   
-      ((TextView) layout.findViewById(R.id.TextViewSortieListItemAdresse)).setText(poi.voie+" "+ poi.codepostal+" "+poi.ville);
-    
-    return layout;
-  }
+		((TextView) layout.findViewById(R.id.TextViewSortieListItemTitle))
+				.setText(poi.raisonsociale);
+
+		// ((TextView)
+		// layout.findViewById(R.id.TextViewSortieListItemAdresse)).setText(poi.voie+" "+
+		// poi.codepostal+" "+poi.ville);
+		((TextView) layout.findViewById(R.id.TextViewSortieListItemAdresse))
+				.setText(poi.theme);
+
+		return layout;
+	}
 }
