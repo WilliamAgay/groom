76b5f29dec7f8ac2ddd58c4d1c9c06ea91f6df1a
Audrey Martel <adry.ma@gmail.com>
2012-11-10 12:21:16 +0100

    layout init

--- gen/com/opendata/groom/R.java
+++ gen/com/opendata/groom/R.java
@@ -9,81 +9,134 @@ package com.opendata.groom;
 
 public final class R {
     public static final class anim {
-        public static final int translate_in=0x7f040000;
+        public static final int polaris__grow_fade_in_from_bottom=0x7f040000;
+        public static final int polaris__shrink_fade_out_to_bottom=0x7f040001;
+        public static final int translate_in=0x7f040002;
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int polaris__primary=0x7f060000;
+        public static final int polaris__secondary=0x7f060001;
+    }
+    public static final class dimen {
+        public static final int polaris__font_size_normal=0x7f070004;
+        public static final int polaris__font_size_small=0x7f070003;
+        public static final int polaris__spacing_large=0x7f070002;
+        public static final int polaris__spacing_normal=0x7f070001;
+        public static final int polaris__spacing_small=0x7f070000;
+    }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
-        public static final int ic_arrow_white=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ic_arrow_black=0x7f020001;
+        public static final int ic_arrow_white=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int polaris__arrow_to_right=0x7f020004;
+        public static final int polaris__btn_map_overlay=0x7f020005;
+        public static final int polaris__btn_map_overlay_focused=0x7f020006;
+        public static final int polaris__btn_map_overlay_normal=0x7f020007;
+        public static final int polaris__btn_map_overlay_pressed=0x7f020008;
+        public static final int polaris__ic_map_locate=0x7f020009;
+        public static final int polaris__map_callout_bottom_anchor=0x7f02000a;
+        public static final int polaris__map_callout_bottom_anchor_focused=0x7f02000b;
+        public static final int polaris__map_callout_bottom_anchor_normal=0x7f02000c;
+        public static final int polaris__map_callout_bottom_anchor_pressed=0x7f02000d;
+        public static final int polaris__map_callout_left_cap=0x7f02000e;
+        public static final int polaris__map_callout_left_cap_focused=0x7f02000f;
+        public static final int polaris__map_callout_left_cap_normal=0x7f020010;
+        public static final int polaris__map_callout_left_cap_pressed=0x7f020011;
+        public static final int polaris__map_callout_right_cap=0x7f020012;
+        public static final int polaris__map_callout_right_cap_focused=0x7f020013;
+        public static final int polaris__map_callout_right_cap_normal=0x7f020014;
+        public static final int polaris__map_callout_right_cap_pressed=0x7f020015;
     }
     public static final class id {
-        public static final int ButtonInitActivityGo=0x7f08000c;
-        public static final int CheckBoxInitActivityChoix1=0x7f080024;
-        public static final int EditTextQuestionLayoutR=0x7f080027;
-        public static final int FrameLayoutQuestionLayout=0x7f080021;
-        public static final int History=0x7f080000;
-        public static final int ImageViewInitActivitySettings=0x7f08001f;
-        public static final int IncludeInitActivityTile1=0x7f080011;
-        public static final int IncludeInitActivityTile2=0x7f080012;
-        public static final int IncludeInitActivityTile3=0x7f080016;
-        public static final int IncludeInitActivityTile4=0x7f080017;
-        public static final int IncludeInitActivityTile5=0x7f08001b;
-        public static final int IncludeInitActivityTile6=0x7f08001c;
-        public static final int InputMessage=0x7f080001;
-        public static final int LinearLayoutInitActivityContainerFooter=0x7f08000e;
-        public static final int LinearLayoutInitActivityContainerHeader=0x7f080009;
-        public static final int LinearLayoutInitActivityContainerTiles=0x7f08000d;
-        public static final int RelativeLayoutInitActivityChoix1=0x7f080022;
-        public static final int RelativeLayoutInitActivityContainer=0x7f080008;
-        public static final int RelativeLayoutInitActivityFooterContainer=0x7f08001e;
-        public static final int RelativeLayoutInitActivityHeaderContainer=0x7f08000a;
-        public static final int RelativeLayoutInitActivityLine1Tiles=0x7f08000f;
-        public static final int RelativeLayoutInitActivityLine2Tiles=0x7f080014;
-        public static final int RelativeLayoutInitActivityLine3Tiles=0x7f080019;
-        public static final int RelativeLayoutQuestionLayout=0x7f080025;
-        public static final int TextViewInitActivityChat=0x7f080020;
-        public static final int TextViewInitActivityChoix1=0x7f080023;
-        public static final int TextViewInitActivityHeader=0x7f08000b;
-        public static final int TextViewQuestionLayoutQ=0x7f080026;
-        public static final int ViewInitActivityLine1Separator=0x7f080013;
-        public static final int ViewInitActivityLine1TilesSeparator=0x7f080010;
-        public static final int ViewInitActivityLine2Separator=0x7f080018;
-        public static final int ViewInitActivityLine2TilesSeparator=0x7f080015;
-        public static final int ViewInitActivityLine3Separator=0x7f08001d;
-        public static final int ViewInitActivityLine3TilesSeparator=0x7f08001a;
-        public static final int boutonChat=0x7f080007;
-        public static final int btn_Send=0x7f080005;
-        public static final int listView=0x7f080002;
-        public static final int llout=0x7f080003;
-        public static final int menu_settings=0x7f080028;
-        public static final int textView1=0x7f080006;
-        public static final int txt_inputText=0x7f080004;
+        public static final int ButtonInitActivityGo=0x7f0b000c;
+        public static final int ButtonQuestionLayoutGo=0x7f0b002e;
+        public static final int CheckBoxInitActivityChoix1=0x7f0b0024;
+        public static final int EditTextQuestionLayoutR=0x7f0b002d;
+        public static final int FrameLayoutQuestionLayout=0x7f0b0021;
+        public static final int History=0x7f0b0000;
+        public static final int ImageViewInitActivitySettings=0x7f0b001f;
+        public static final int IncludeInitActivityTile1=0x7f0b0011;
+        public static final int IncludeInitActivityTile2=0x7f0b0012;
+        public static final int IncludeInitActivityTile3=0x7f0b0016;
+        public static final int IncludeInitActivityTile4=0x7f0b0017;
+        public static final int IncludeInitActivityTile5=0x7f0b001b;
+        public static final int IncludeInitActivityTile6=0x7f0b001c;
+        public static final int InputMessage=0x7f0b0001;
+        public static final int LinearLayoutInitActivityContainerFooter=0x7f0b000e;
+        public static final int LinearLayoutInitActivityContainerHeader=0x7f0b0009;
+        public static final int LinearLayoutInitActivityContainerTiles=0x7f0b000d;
+        public static final int RelativeLayoutInitActivityChoix1=0x7f0b0022;
+        public static final int RelativeLayoutInitActivityContainer=0x7f0b0008;
+        public static final int RelativeLayoutInitActivityFooterContainer=0x7f0b001e;
+        public static final int RelativeLayoutInitActivityHeaderContainer=0x7f0b000a;
+        public static final int RelativeLayoutInitActivityLine1Tiles=0x7f0b000f;
+        public static final int RelativeLayoutInitActivityLine2Tiles=0x7f0b0014;
+        public static final int RelativeLayoutInitActivityLine3Tiles=0x7f0b0019;
+        public static final int RelativeLayoutQuestionLayout=0x7f0b002b;
+        public static final int TextViewInitActivityChat=0x7f0b0020;
+        public static final int TextViewInitActivityChoix1=0x7f0b0023;
+        public static final int TextViewInitActivityHeader=0x7f0b000b;
+        public static final int TextViewQuestionLayoutQ=0x7f0b002c;
+        public static final int ViewInitActivityLine1Separator=0x7f0b0013;
+        public static final int ViewInitActivityLine1TilesSeparator=0x7f0b0010;
+        public static final int ViewInitActivityLine2Separator=0x7f0b0018;
+        public static final int ViewInitActivityLine2TilesSeparator=0x7f0b0015;
+        public static final int ViewInitActivityLine3Separator=0x7f0b001d;
+        public static final int ViewInitActivityLine3TilesSeparator=0x7f0b001a;
+        public static final int boutonChat=0x7f0b0007;
+        public static final int btn_Send=0x7f0b0005;
+        public static final int listView=0x7f0b0002;
+        public static final int llout=0x7f0b0003;
+        public static final int menu_settings=0x7f0b0030;
+        public static final int pager=0x7f0b002f;
+        public static final int polaris__callout=0x7f0b0025;
+        public static final int polaris__content=0x7f0b0027;
+        public static final int polaris__content_container=0x7f0b0026;
+        public static final int polaris__disclosure=0x7f0b002a;
+        public static final int polaris__subtitle=0x7f0b0029;
+        public static final int polaris__title=0x7f0b0028;
+        public static final int textView1=0x7f0b0006;
+        public static final int txt_inputText=0x7f0b0004;
+    }
+    public static final class interpolator {
+        public static final int polaris__decelerate_cubic=0x7f050000;
+        public static final int polaris__decelerate_quint=0x7f050001;
     }
     public static final class layout {
         public static final int activity_chat=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int init_activity=0x7f030002;
         public static final int init_layout_tile=0x7f030003;
-        public static final int question_layout=0x7f030004;
+        public static final int polaris__map_callout_view_merge=0x7f030004;
+        public static final int polaris__user_tracking_button=0x7f030005;
+        public static final int question_layout=0x7f030006;
+        public static final int tab_actiity_fragment=0x7f030007;
     }
     public static final class menu {
-        public static final int activity_chat=0x7f070000;
-        public static final int activity_main=0x7f070001;
+        public static final int activity_chat=0x7f0a0000;
+        public static final int activity_main=0x7f0a0001;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int title_activity_chat=0x7f050004;
-        public static final int title_activity_main=0x7f050003;
+        public static final int app_name=0x7f080002;
+        public static final int hello_world=0x7f080003;
+        public static final int menu_settings=0x7f080004;
+        public static final int polaris__locate_myself=0x7f080000;
+        public static final int polaris__unable_to_locate_you=0x7f080001;
+        public static final int title_activity_chat=0x7f080006;
+        public static final int title_activity_main=0x7f080005;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
-        public static final int Theme_Transparent=0x7f060001;
-        public static final int styleSettingsTxt=0x7f060002;
-        public static final int styleSettingsTxtTilesTitre2=0x7f060004;
-        public static final int styleSettingsTxtTitre=0x7f060003;
+        public static final int AppTheme=0x7f090004;
+        public static final int TextAppearance_Polaris=0x7f090000;
+        public static final int TextAppearance_Polaris_MapCallout=0x7f090001;
+        public static final int TextAppearance_Polaris_MapCallout_Subtitle=0x7f090003;
+        public static final int TextAppearance_Polaris_MapCallout_Title=0x7f090002;
+        public static final int Theme_Transparent=0x7f090005;
+        public static final int styleSettingsTxt=0x7f090006;
+        public static final int styleSettingsTxtTilesTitre2=0x7f090008;
+        public static final int styleSettingsTxtTitre=0x7f090007;
     }
 }
--- src/com/opendata/groom/activities/InitActivity.java
+++ src/com/opendata/groom/activities/InitActivity.java
@@ -1,18 +1,26 @@
 package com.opendata.groom.activities;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import android.R.bool;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.opendata.groom.R;
 
-public class InitActivity extends Activity
+public class InitActivity extends Activity implements View.OnClickListener
 {
+
+	List<String> questions = Arrays.asList("Votre prénom?", "Situation : ", "Votre budget?");
 
 	Handler mHandler = new Handler();
 	
@@ -39,12 +47,31 @@ public class InitActivity extends Activity
 				@Override
 				public void run() {
 					findViewById(R.id.FrameLayoutQuestionLayout).setVisibility(View.VISIBLE);
+					((TextView)findViewById(R.id.TextViewQuestionLayoutQ)).setText(questions.get(0));
+					findViewById(R.id.ButtonQuestionLayoutGo).setOnClickListener(InitActivity.this);
 					
 					Animation scaleDimAnimation = AnimationUtils.loadAnimation(InitActivity.this, R.anim.translate_in);
 					findViewById(R.id.FrameLayoutQuestionLayout).startAnimation(scaleDimAnimation);
 					hasDone=true;
 				}
 			}, 1000);
+		}
+	}
+
+	
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.ButtonQuestionLayoutGo:
+			if()
+			{
+				
+			}
+			break;
+
+		default:
+			break;
 		}
 	}
 	
