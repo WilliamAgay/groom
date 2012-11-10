d874f5dc64b628a0804f93ea0ab8d85fc0135876
Audrey Martel <adry.ma@gmail.com>
2012-11-10 12:06:18 +0100

    layout init

--- gen/com/opendata/groom/R.java
+++ gen/com/opendata/groom/R.java
@@ -8,6 +8,9 @@
 package com.opendata.groom;
 
 public final class R {
+    public static final class anim {
+        public static final int translate_in=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -16,57 +19,71 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int ButtonInitActivityGo=0x7f07000c;
-        public static final int CheckBoxInitActivityChoix1=0x7f07001a;
-        public static final int History=0x7f070000;
-        public static final int ImageViewInitActivitySettings=0x7f070016;
-        public static final int InputMessage=0x7f070001;
-        public static final int LinearLayoutInitActivityContainerFooter=0x7f07000e;
-        public static final int LinearLayoutInitActivityContainerHeader=0x7f070009;
-        public static final int LinearLayoutInitActivityContainerTiles=0x7f07000d;
-        public static final int RelativeLayoutInitActivityChoix1=0x7f070018;
-        public static final int RelativeLayoutInitActivityContainer=0x7f070008;
-        public static final int RelativeLayoutInitActivityFooterContainer=0x7f070015;
-        public static final int RelativeLayoutInitActivityHeaderContainer=0x7f07000a;
-        public static final int RelativeLayoutInitActivityLine1Tiles=0x7f07000f;
-        public static final int RelativeLayoutInitActivityLine2Tiles=0x7f070011;
-        public static final int RelativeLayoutInitActivityLine3Tiles=0x7f070013;
-        public static final int TextViewInitActivityChat=0x7f070017;
-        public static final int TextViewInitActivityChoix1=0x7f070019;
-        public static final int TextViewInitActivityHeader=0x7f07000b;
-        public static final int ViewInitActivityLine1TilesSeparator=0x7f070010;
-        public static final int ViewInitActivityLine2TilesSeparator=0x7f070012;
-        public static final int ViewInitActivityLine3TilesSeparator=0x7f070014;
-        public static final int boutonChat=0x7f070007;
-        public static final int btn_Send=0x7f070005;
-        public static final int listView=0x7f070002;
-        public static final int llout=0x7f070003;
-        public static final int menu_settings=0x7f07001b;
-        public static final int textView1=0x7f070006;
-        public static final int txt_inputText=0x7f070004;
+        public static final int ButtonInitActivityGo=0x7f08000c;
+        public static final int CheckBoxInitActivityChoix1=0x7f080024;
+        public static final int EditTextQuestionLayoutR=0x7f080027;
+        public static final int FrameLayoutQuestionLayout=0x7f080021;
+        public static final int History=0x7f080000;
+        public static final int ImageViewInitActivitySettings=0x7f08001f;
+        public static final int IncludeInitActivityTile1=0x7f080011;
+        public static final int IncludeInitActivityTile2=0x7f080012;
+        public static final int IncludeInitActivityTile3=0x7f080016;
+        public static final int IncludeInitActivityTile4=0x7f080017;
+        public static final int IncludeInitActivityTile5=0x7f08001b;
+        public static final int IncludeInitActivityTile6=0x7f08001c;
+        public static final int InputMessage=0x7f080001;
+        public static final int LinearLayoutInitActivityContainerFooter=0x7f08000e;
+        public static final int LinearLayoutInitActivityContainerHeader=0x7f080009;
+        public static final int LinearLayoutInitActivityContainerTiles=0x7f08000d;
+        public static final int RelativeLayoutInitActivityChoix1=0x7f080022;
+        public static final int RelativeLayoutInitActivityContainer=0x7f080008;
+        public static final int RelativeLayoutInitActivityFooterContainer=0x7f08001e;
+        public static final int RelativeLayoutInitActivityHeaderContainer=0x7f08000a;
+        public static final int RelativeLayoutInitActivityLine1Tiles=0x7f08000f;
+        public static final int RelativeLayoutInitActivityLine2Tiles=0x7f080014;
+        public static final int RelativeLayoutInitActivityLine3Tiles=0x7f080019;
+        public static final int RelativeLayoutQuestionLayout=0x7f080025;
+        public static final int TextViewInitActivityChat=0x7f080020;
+        public static final int TextViewInitActivityChoix1=0x7f080023;
+        public static final int TextViewInitActivityHeader=0x7f08000b;
+        public static final int TextViewQuestionLayoutQ=0x7f080026;
+        public static final int ViewInitActivityLine1Separator=0x7f080013;
+        public static final int ViewInitActivityLine1TilesSeparator=0x7f080010;
+        public static final int ViewInitActivityLine2Separator=0x7f080018;
+        public static final int ViewInitActivityLine2TilesSeparator=0x7f080015;
+        public static final int ViewInitActivityLine3Separator=0x7f08001d;
+        public static final int ViewInitActivityLine3TilesSeparator=0x7f08001a;
+        public static final int boutonChat=0x7f080007;
+        public static final int btn_Send=0x7f080005;
+        public static final int listView=0x7f080002;
+        public static final int llout=0x7f080003;
+        public static final int menu_settings=0x7f080028;
+        public static final int textView1=0x7f080006;
+        public static final int txt_inputText=0x7f080004;
     }
     public static final class layout {
         public static final int activity_chat=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int init_activity=0x7f030002;
         public static final int init_layout_tile=0x7f030003;
+        public static final int question_layout=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_chat=0x7f060000;
-        public static final int activity_main=0x7f060001;
+        public static final int activity_chat=0x7f070000;
+        public static final int activity_main=0x7f070001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_chat=0x7f040004;
-        public static final int title_activity_main=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_chat=0x7f050004;
+        public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
-        public static final int Theme_Transparent=0x7f050001;
-        public static final int styleSettingsTxt=0x7f050002;
-        public static final int styleSettingsTxtTilesTitre2=0x7f050004;
-        public static final int styleSettingsTxtTitre=0x7f050003;
+        public static final int AppTheme=0x7f060000;
+        public static final int Theme_Transparent=0x7f060001;
+        public static final int styleSettingsTxt=0x7f060002;
+        public static final int styleSettingsTxtTilesTitre2=0x7f060004;
+        public static final int styleSettingsTxtTitre=0x7f060003;
     }
 }
--- src/com/opendata/groom/activities/InitActivity.java
+++ src/com/opendata/groom/activities/InitActivity.java
@@ -1,25 +1,51 @@
 package com.opendata.groom.activities;
 
 
-import com.opendata.groom.R;
-
+import android.R.bool;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.opendata.groom.R;
 
 public class InitActivity extends Activity
 {
 
+	Handler mHandler = new Handler();
+	
+	boolean hasDone = false;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.init_activity);
+		findViewById(R.id.FrameLayoutQuestionLayout).setVisibility(View.GONE);
+		
+		
 	}
 
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
+		if(!hasDone)
+		{
+			mHandler.postDelayed(new Runnable() {
+				
+				@Override
+				public void run() {
+					findViewById(R.id.FrameLayoutQuestionLayout).setVisibility(View.VISIBLE);
+					
+					Animation scaleDimAnimation = AnimationUtils.loadAnimation(InitActivity.this, R.anim.translate_in);
+					findViewById(R.id.FrameLayoutQuestionLayout).startAnimation(scaleDimAnimation);
+					hasDone=true;
+				}
+			}, 1000);
+		}
 	}
 	
 	
