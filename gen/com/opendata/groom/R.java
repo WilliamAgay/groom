73ca57a72ec658550794cb176b6d39ea0430826e
thannous <thannous@gmail.com>
2012-11-10 04:25:01 +0100

    Signed-off-by: thannous <thannous@gmail.com>
    just un bouton pour aller dans le chat
    
    Signed-off-by: thannous <thannous@gmail.com>

--- gen/com/opendata/groom/R.java
+++ gen/com/opendata/groom/R.java
@@ -15,7 +15,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int History=0x7f070000;
+        public static final int InputMessage=0x7f070001;
+        public static final int boutonChat=0x7f070007;
+        public static final int btn_Send=0x7f070005;
+        public static final int listView=0x7f070002;
+        public static final int llout=0x7f070003;
+        public static final int menu_settings=0x7f070008;
+        public static final int textView1=0x7f070006;
+        public static final int txt_inputText=0x7f070004;
     }
     public static final class layout {
         public static final int activity_chat=0x7f030000;
--- src/com/opendata/groom/Main.java
+++ src/com/opendata/groom/Main.java
@@ -2,19 +2,29 @@ package com.opendata.groom;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends Activity {
-
+	private Button boutonChat;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        boutonChat = (Button) findViewById(R.id.boutonChat);
+        boutonChat.setOnClickListener(new View.OnClickListener(){
+        	
+        @Override
+        public void onClick(View v) {
+    	Intent intent = new Intent(getApplicationContext(),ChatActivity.class);
+        startActivity(intent)
+;        }
+    });
+    
+    
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+  
     }
 }
